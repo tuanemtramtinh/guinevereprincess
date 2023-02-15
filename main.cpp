3bcbd134cb6887aa27ed6b9ca8b9d08317dafe35
TuanEmTramTinh <anhnguyentuan073@gmail.com>
2023-02-15 23:14:21 +0700

    Finish Boss 7

--- main.cpp
+++ main.cpp
@@ -5,5 +5,4 @@ int main(int argc, char ** argv) {
     int HP, level, remedy, maidenkiss, phoenixdown, rescue;
     adventureToKoopa(file_input, HP, level, remedy, maidenkiss, phoenixdown, rescue);
     return 0;
-
 }
